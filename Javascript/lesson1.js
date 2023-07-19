a214c90820a3b0bc95ccbce1ccc79608b10b06a8
Omar <omarsahal22@outlook.com>
2023-07-19 07:14:11 +0100

    media queries and javascript

---
+++ Javascript/lesson1.js
@@ -0,0 +1,13 @@
+
+var word="Hello"
+var second="2"
+console.log(word);
+console.log(second);
+
+var name="Omar";
+var age="23";
+var dob="2000";
+console.log("Hello, my name is",name ,"and im ", age, "years old and born in the ", dob );
+var number =23;
+var secondNumber=60;
+console.log(number + secondNumber);
